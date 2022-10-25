0445fe5bfec671ac3aa407f7b123913ba7540d9f
Kallebe Nunes <kallebe@maeztra.com>
2022-10-25 02:38:18 -0300

    feat: configure router and create edit and add sector pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-import { Col, Layout, Row } from 'antd'
-import { Content, Header } from 'antd/lib/layout/layout'
-import Sider from 'antd/lib/layout/Sider'
-import SectorSelector from './components/SectorSelector'
-
-
+import { Col, Layout, Row } from "antd";
+import { Content, Header } from "antd/lib/layout/layout";
+import Sider from "antd/lib/layout/Sider";
+import SectorSelector from "./components/SectorSelector";
+import { Route, Router, Routes } from "react-router";
+import Edit from './pages/EditSector';
+import CreateSector from "./pages/CreateSector";
+import EditSector from "./pages/EditSector";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header/>
-    <Row style={{height: "calc(100vh - 64px)"}}>
-      <Col style={{background: ""}} lg={{span: 8}}>
-        <SectorSelector/>
-      </Col>
-      <Col  >  
-        teste
-      </Col>
-    </Row>
+      <Header />
+
+      <Row style={{ height: "calc(100vh - 64px)" }}>
+        <Col style={{ background: "" }} lg={{ span: 8 }}>
+          <SectorSelector />
+        </Col>
+        <Col>
+        <Routes>
+            <Route path="/" element={<CreateSector />} />
+            <Route path="/edit" element={<EditSector />} />
+          </Routes>
+        </Col>
+      </Row>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "antd/dist/antd.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Switch } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
